d0314ed45b8f6866fa6db06d335ab2fcc35e9397
Mainak Chakraborty <76518189+Mainak1792@users.noreply.github.com>
2025-03-22 15:43:15 +0530

    Update and rename eslint.config.mjs to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// .eslintrc.js
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'next/typescript'
+  ],
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    // add any other rule overrides here
+  },
+};
