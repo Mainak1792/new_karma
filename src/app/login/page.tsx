9eddf82e54624c70345ea557d3f2ce12a422db35
VIBeID <vibeidnips@gmail.com>
2025-03-24 10:15:35 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
     <div className="mt-20 flex flex-col items-center justify-center">
       <Card className="w-full max-w-md">
         <CardHeader className="flex flex-col items-center justify-center space-y-2">
-          <CardTitle className="text-2xl font-bold text-center">Made with ❤️ in New-Delhi</CardTitle>
+          <CardTitle className="text-2xl font-bold text-center">Made with ❤️ in New-Delhi 🛺</CardTitle>
           <CardDescription className="text-muted-foreground text-center">
             Copyright © 2025 All rights by <a href="https://www.mainakchakraborty.com/" className="text-blue-500 hover:text-blue-600">Mainak</a>
           </CardDescription>
